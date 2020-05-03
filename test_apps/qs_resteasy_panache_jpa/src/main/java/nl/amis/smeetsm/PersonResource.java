3d63160a5c5968ed796c6beb0210f7ab5c460625
John OHara <johara@redhat.com>
2020-05-03 15:26:43 +0100

    Add resteasy jpa test

---
+++ test_apps/qs_resteasy_panache_jpa/src/main/java/nl/amis/smeetsm/PersonResource.java
@@ -0,0 +1,36 @@
+package nl.amis.smeetsm;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("people")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PersonResource {
+
+    @GET
+    public List<Person> get() {
+        return Person.findAll().list();
+    }
+
+    @GET
+    @Path("{id}")
+    public Person getSingle(@PathParam("id") Long id) {
+        return Person.findById(id);
+    }
+
+    @POST
+    @Transactional
+    public void create(Person person) {
+        person.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public long delete(@PathParam("id") Long id) {
+        return Person.delete("id",id);
+    }
+}
